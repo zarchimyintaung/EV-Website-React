3f509c69806c74ac4a72247e14fb0cdeaa3cf88e
zarchimyintaung <zarchimyintaung12@gmail.com>
2026-03-18 17:25:20 +0630

    Add Hero

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,13 @@ const App = () => {
     <div>
       <Background playStatus={playStatus} heroCount={heroCount}/>
       <NavBar/>
+      <Hero
+        setPlayStatus={setPlayStatus}
+        heroData={heroData[heroCount]}
+        heroCount={heroCount}
+        setHeroCount={setHeroCount}
+        playStatus={playStatus}
+      />
     </div>
   )
 }
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,9 +1,27 @@
 import './Hero.css';
+import arrow_btn from '../../assets/arrow_btn.png';
+import play_icon from '../../assets/play_icon.png';
+import pause_icon from '../../assets/pause_icon.png';
 
-const Hero = () => {
+
+const Hero = ({heroData,setHeroCount,heroCount,setPlayStatus,playStatus}) => {
   return (
     <div className='hero'>
-      
+      <div className="hero-text">
+        <p>{heroData.text1}</p>
+        <p>{heroData.text2}</p>
+      </div>
+      <div className="hero-explore">
+        <p>Explore The Feature</p>
+        <img src={arrow_btn} alt="" />
+      </div>
+      <div className="hero-dot-play">
+        <ul className="hero-dots">
+          <li className={heroCount===0?"hero-dot orange":"hero-dot"}></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
     </div>
   )
 }
